6272bdf24418cbb4ffe3efcd7a17b23df11bd812
Rex Zeng <rex.fgtsky@qq.com>
2016-09-22 13:53:13 +0800

    优化 indexcfg 的速度

--- api/1/indexcfg.php
+++ api/1/indexcfg.php
@@ -2,7 +2,7 @@
 
 /**
 * @file indexcfg.php
-* @Brief 
+* @Brief
 * @author tangyy
 * @version 1
 * @date 2015-09-22
@@ -23,45 +23,36 @@ $discuz->init();
 $max_button_num = 5;
 $button_configs = array();
 
+require_once libfile('function/cache');
+updatecache('setting');
+
 for($i = 1; $i<=$max_button_num; $i++) {
-	$key = "bigapp_button_id_" . $i . "_setting";
-	
 	global $_G;
-	
-	require_once libfile('function/cache');
-	updatecache('setting');
+	$key = "bigapp_button_id_{$i}_setting";
+
 	if(isset($_G['setting'][$key]) && !empty($_G['setting'][$key])){
 		$succRet = unserialize($_G['setting'][$key]);
 	} else {
 		$succRet = C::t('common_setting')->fetch($key, true);
 	}
-	
-	
+
 	if(isset($succRet[0]) && empty($succRet[0])) {
 		$succRet = AppDesign::getDefaultButtonSetting($i);
-		$succRet= AppDesign::procFrontData($succRet);####################
+		$succRet = AppDesign::procFrontData($succRet); ####################
 	}
-	
+
 	$button_config = array(
 		"id" => $succRet['id'],
 		"button_type" => $succRet['button_type'],
 		"button_name" => $succRet['name'],
 		"icon_type" => $succRet['icon_type'],
-		//"tab_cfg" => AppDesign::getTabCfgInfo($succRet),
+		"tab_cfg" => AppDesign::getTabCfgInfo($succRet)
 	);
 
-   $ret = AppDesign::getTabCfgInfo($succRet); #########################
-   
-	if(!empty($ret)) {
-		$button_config['tab_cfg'] = AppDesign::getTabCfgInfo($succRet);
-	}
-	
 	array_push($button_configs, $button_config);
 }
 
 $variable = array (
 	"button_configs" => $button_configs,
 );
-
 bigapp_core::result(bigapp_core::variable($variable));
-?>
